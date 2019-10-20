534754c559a9ad60081218b13f5ebb89209e7ddb
Pratik-sys <pithore.pratik@gmail.com>
2019-10-20 22:08:21 +0530

    add the info regarding the code

--- ascii_to_int.py
+++ ascii_to_int.py
@@ -1,8 +1,10 @@
-rat = input("enter the ASCII code:")
-p1 = []
-r2 = []
-for i in rat:
-    p1.append(i)
-    r2.append(ord(i))
+# Question: How to convert a string to a number that consists of letters ASCII code. Example: 'abcd' -> 979899100. Write a program for this.
+
+# Solution:
+
+rat = input("enter the ASCII code:") 
+p1 = list(rat)
+r2 = [ord(i) for i in rat]
+
 print(p1)
 print(r2)
