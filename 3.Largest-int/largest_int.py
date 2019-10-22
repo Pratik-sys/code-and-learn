088b31b5f3be255d4605cb18c0fc860b9977ed98
Prathamesh Pathak <prathameshpathak@Prathameshs-MacBook-Air.local>
2019-10-22 21:32:35 +0530

    - Modify folder structure for multiple solutions.

---
+++ 3.Largest-int/largest_int.py
@@ -0,0 +1,12 @@
+# Given a list of numbers(unsorted), write a praogram to write a Python Program to find the largerst number in the given list.
+# Write various methods with Justification
+"""
+Input : A = [14,2,8, 90]
+Output: 90
+
+Input : B = [56, 31, 40, 9]
+Output: 56
+"""
+
+
+
