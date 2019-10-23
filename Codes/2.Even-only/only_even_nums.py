9d185e374200c8ad43e0dd7e6545eeb7bcfc6bc6
Pratik-sys <pithore.pratik@gmail.com>
2019-10-23 14:09:54 +0530

    Codes: update the only_even_nums
    Signed-off-by: Pratik-sys <pithore.pratik@gmail.com>

--- Codes/2.Even-only/only_even_nums.py
+++ Codes/2.Even-only/only_even_nums.py
@@ -1,5 +1,17 @@
-# Given a list of numbers, write a python program to print all the numbers in a given list.
+#!/usr/bin/python3
+# Given a list of numbers, write a python program to print all the even numbers in a given list.
 '''
 Input : A = [1,2,3,4,5,6,7,8]
 Output : [2,4,6,8]
 '''
+
+def even_num(inp_list):
+    '''
+    This function returns the even numbers from the given list 
+    '''
+    even_list = list(inp_list)
+    for num in even_list:
+        if int(num) % 2 == 0:
+            print(num, end=" ")
+
+even_num(input('enter the list:'))
