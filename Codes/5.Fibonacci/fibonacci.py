f2989db23770b40ae350518878147ffccd9ad5cb
Prathamesh Pathak <prathameshpathak@Prathameshs-MacBook-Air.local>
2019-10-25 23:37:21 +0530

    Add fibonacci to the list

---
+++ Codes/5.Fibonacci/fibonacci.py
@@ -0,0 +1,7 @@
+# Write a function int fib(int n) that returns Fn. For example, if n = 0, then fib() should return 0. If n = 1, then it should return 1. For n > 1, it should return Fn-1 + Fn-2.
+
+
+"""
+Input: 9
+Output : 34
+"""
