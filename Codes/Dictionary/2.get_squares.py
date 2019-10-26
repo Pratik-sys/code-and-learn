c4cfca7a71351ba4d8ccbc559d70fcd0a0ff241f
Prathamesh Pathak <prathameshpathak@Prathameshs-MacBook-Air.local>
2019-10-26 22:39:58 +0530

    Add Dictionary to the list

---
+++ Codes/Dictionary/2.get_squares.py
@@ -0,0 +1,14 @@
+# Question
+# Define a function which can print a dictionary where the keys are numbers between 2 and 20 (both included) and the values are square of keys. Bonus for writing another function which only print values. Hint: items()
+
+'''
+Expected Output: {2:4,3:9....20:400}
+'''
+def square_print():
+    '''
+    Print the Key value pair, don't return anything
+    '''
+    pass
+
+
+
