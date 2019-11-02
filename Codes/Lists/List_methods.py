a13def24f0213cc26ebee42d8ab98b62158dab1f
Prathamesh Pathak <prathameshpathak@Prathameshs-MacBook-Air.local>
2019-11-02 23:55:42 +0530

    Awarenes for OSS

---
+++ Codes/Lists/List_methods.py
@@ -0,0 +1,6 @@
+## Creating a List
+## Adding Element in List
+## Accessing elements from the list
+## Removing Element from the list
+## Slicing of List
+## 
---
+++ Codes/Lists/list.md
@@ -0,0 +1,4 @@
+
+| Function | Description|
+|--|--|
+|Append|Adding an element ot the end of the list|
