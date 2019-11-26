911cba674c6bb6833b6e8f95dd946ee59bc84cd5
Prathamesh Pathak <pprathamesh1594@gmail.com>
2019-11-27 02:30:50 +0530

    Add String Folder

---
+++ Codes/Strings/1.capital_index.py
@@ -0,0 +1,12 @@
+# Write a function named capital_indexes. The function takes a single parameter, which is a string. Your function should return a list of all the indexes in the string that have capital letters.
+'''
+capital_indexes("HeLlO") :return [0, 2, 4].
+'''
+
+def capital_indexes(s):
+    '''
+    s : string
+    return a: list
+    '''
+    pass
+
