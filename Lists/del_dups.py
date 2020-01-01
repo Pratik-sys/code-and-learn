ff05c28883598f7d8f9e7c25fe3c58a8f57b1512
Prathamesh Pathak <prathameshpathak@Prathameshs-MacBook-Air.local>
2020-01-02 01:43:20 +0530

    Add string+1/Add list+1

---
+++ Lists/del_dups.py
@@ -0,0 +1,17 @@
+# Write a program that takes a list and returns a new list that contains all the elements of the first list minus all the duplicates.
+
+#Write at least two different functions to do this
+# Test Cases
+"""
+    [1,2,3,4,3,4] ---> [1,2,3,4]
+    [5,9,9,9,6] ----> [5,9,6]
+"""
+
+def del_dups_1(x):
+    pass
+
+
+def del_dups_2(x):
+    pass
+
+
---
+++ Strings/reverse_words.py
@@ -0,0 +1,13 @@
+# Write a program (using functions!) that asks the user for a long string containing multiple words. Print back to the user the same string, except with the words in backwards order
+# Avoid using python in-built step slicing, this should work in-place
+
+# Test Cases
+"""
+    'Dark side of Moon' ---> 'Moon of side Dark'
+    'Peace is Myth' ---> 'Myth is Peace'
+"""
+
+
+def reverse_words(strng):
+    pass
+
