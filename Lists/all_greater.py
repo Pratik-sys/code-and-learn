9e5ae456983184578ab5ce1d9f01a8830187fcfd
Prathamesh Pathak <pprathamesh1594@gmail.com>
2020-01-04 13:55:30 +0530

    Add list+2

---
+++ Lists/all_greater.py
@@ -0,0 +1,13 @@
+# Write a program to verify that in the list all the values are greater than the given
+# number k
+# Wiring at-least one pythonic way/function is mandatory
+"""
+    a = [90,80, 70, 40, 20, 65], k = 30 ----> False
+    a = [90,80, 70, 40, 20, 65], k = 10 ----> True
+"""
+
+
+def all_greater(arr, k):
+    pass
+    # return bool
+
---
+++ Lists/pos_of_max.py
@@ -0,0 +1,19 @@
+# Find the positions of maximum element of list.
+# Assume the list  can have multiple maximum elements and hence multiple maximum positions.
+# Return the list with index of max element
+# The catch is to do it one-liner
+
+"""
+    a = [3,9,1,5,8,9] ----> [1,5]
+    a = [20,40,80,80,78,80] ----> [2,3,5]
+   
+"""
+
+
+def pos_max(arr):
+    """
+    arr : list
+    function returns a list of indices of max element
+    """
+    pass
+    # return list of index
