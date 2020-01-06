c7301a68a7ea74b11b32752459c034a2ead2eb34
Prathamesh Pathak <pprathamesh1594@gmail.com>
2020-01-06 14:13:06 +0530

    Add Dictionary+2

---
+++ Dictionary/first_repeated_word.py
@@ -0,0 +1,19 @@
+# Write a program to find first repeated word in a string in Python using(Dictionary).
+# In case of no repition return 'No_reps_found'
+
+
+"""
+Input : "In the light, turing down the machine that broke"
+Output : the
+ 
+Input : "I will die trying"
+Output : 'No_reps_found'
+
+Input : "he had had he"
+Output : he
+"""
+
+
+def firstRepeat(input):
+    pass
+    # return
---
+++ Dictionary/possible_words.py
@@ -0,0 +1,11 @@
+# Write a program to find possible Words using given characters.
+###
+# No repeated character allowed
+###
+
+'''
+Input : word_dict = ["go","bat","me","eat","goal","boy", "run"]
+        arr = ['e','o','b', 'a','m','g', 'l']
+Output : go, me, goal. 
+'''
+
