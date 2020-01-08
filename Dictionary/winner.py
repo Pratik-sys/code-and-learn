5649903b1e81f406ce82d6b7f0a666f15c2d42f4
Prathamesh Pathak <pprathamesh1594@gmail.com>
2020-01-08 14:26:28 +0530

    Add Dictionary+1/Add Modules+1

---
+++ Dictionary/winner.py
@@ -0,0 +1,20 @@
+# Given an array of names of candidates in an election. A candidate name in array represents a vote casted to the candidate. Print the name of candidates received Max vote. If there is tie, print lexicographically smaller name.
+# Check lexographically smaller/greater on wiki.
+
+"""
+Input :  votes[] = {"john", "johnny", "jackie", 
+                    "johnny", "john", "jackie", 
+                    "jamie", "jamie", "john",
+                    "johnny", "jamie", "johnny", 
+                    "john"};
+Output : 'john'
+
+We have four Candidates with name as 
+'John', 'Johnny', 'jamie', 'jackie'. 
+The candidates John and Johny get maximum votes.
+Since John is alphabetically smaller, we print it.
+"""
+
+
+def winner(input):
+    pass
---
+++ Modules/password_gen.py
@@ -0,0 +1,9 @@
+# Write a password generator in Python.
+# Be creative with how you generate passwords - strong passwords have a mix of lowercase letters, uppercase letters, numbers, and symbols.
+# The passwords should be random, generating a new password every time the user asks for a new password.
+
+"""
+No test cases. 
+This question is left open-ended as it may vary various implemention.
+Using two or more different modules is allowed. 
+"""
