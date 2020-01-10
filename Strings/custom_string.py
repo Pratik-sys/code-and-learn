3f6c5fc3b270e5886a4295776c9e240324d5a060
Prathamesh Pathak <pprathamesh1594@gmail.com>
2020-01-10 14:54:57 +0530

    Add Strings+2

---
+++ Strings/custom_string.py
@@ -0,0 +1,15 @@
+# Given a string 'S' , u need to tell whether it is 'customs string or not'.
+# A string is called 'customs String' , if distance between adjacent character is 1.
+
+# Consider that the alphabets are arranged in cyclic manner from 'a' to 'z'. distance between any character 'x' and 'y' will be defined as minimum number of steps it takes 'x' to reach 'y'. Here, character 'x' can  start moving clockwise or anti-clockwise in order to reach at position where character 'y' is placed.
+
+# Print 'YES' if it is customs string else print 'NO', for each test case.
+
+"""
+aba ---> YES
+zza ---> NO
+bcd ---> YES
+"""
+
+def custom_string(strng):
+    pass
---
+++ Strings/del_dups_str.py
@@ -0,0 +1,16 @@
+# Given a string S. Your task is to remove all duplicates characters from the string S
+#
+# Note:
+# 1.) Order of characters in output string should be same as given in input string.
+# 2.) String S contains only lowercase characters ['a'-'z'].
+
+"""
+programming  ---> progamin
+disco ---> disco
+ramayan ---> ramyn 
+"""
+
+
+def remove_dup_string(strng):
+    pass
+
