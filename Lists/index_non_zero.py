647b04f43208b12ea96f53e998f8143f1acea9cc
Prathamesh Pathak <pprathamesh1594@gmail.com>
2020-01-12 15:25:35 +0530

    Add List+2

---
+++ Lists/index_non_zero.py
@@ -0,0 +1,6 @@
+# Index of Non-Zero elements in a list
+# Write a program find positions of all the integers other than 0.
+
+"""
+[6, 7, 0, 1, 0, 2, 0, 12] --->  [0, 1, 3, 5, 7]
+"""
---
+++ Lists/transpose.py
@@ -0,0 +1,22 @@
+# Given a 2D array A, your task is to convert all rows to columns and columns to rows.
+
+
+"""
+Input:
+[
+    [13 4 8 14 1],
+    [9 6 3 7 21],       
+    [5 12 17 9 3]
+]
+
+Output
+    |
+    v
+ [
+    [13 9 5],
+    [4 6 12],
+    [8 3 17],
+    [14 7 9],
+    [1 21 3]
+ ]
+"""
