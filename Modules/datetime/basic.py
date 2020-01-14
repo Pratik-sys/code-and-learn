c6a5fc19c39ff6935a93c61579436354a10ebc85
Prathamesh Pathak <pprathamesh1594@gmail.com>
2020-01-14 16:04:44 +0530

    Add List+1/Add modules+2

---
+++ Modules/datetime/basic.py
@@ -0,0 +1,1 @@
+# define and explain with examples important methods inside the datetime module.
---
+++ Modules/sorted_random_list.py
@@ -0,0 +1,17 @@
+# Get a sorted list of random integers with unique elements
+# Given lower and upper limits, generate a sorted list of random numbers with unique elements, starting from start to end.
+
+"""
+Input: num:10, start:100, end:200
+Output: [102, 118, 124, 131, 140, 148, 161, 166, 176, 180]
+
+Input: num:5, start:1, end:100
+Output: [37, 49, 64, 84, 95]
+"""
+
+def sorted_random(num, start=default_value, end=default_value):
+    '''
+    To-do: Change a default value to the desirable value
+    start < end, always
+    '''
+    pass
