e49297286b982034b6ea3af6502d6358d85b4925
Pratik-sys <pithore.pratik@gmail.com>
2020-01-18 16:45:57 +0530

    List:update_soln:del_duplicate
    Signed-off-by: Pratik-sys <pithore.pratik@gmail.com>

--- Lists/del_dups.py
+++ Lists/del_dups.py
@@ -1,17 +1,30 @@
 # Write a program that takes a list and returns a new list that contains all the elements of the first list minus all the duplicates.
 
-#Write at least two different functions to do this
+# Write at least two different functions to do this
 # Test Cases
 """
     [1,2,3,4,3,4] ---> [1,2,3,4]
     [5,9,9,9,6] ----> [5,9,6]
 """
 
+
 def del_dups_1(x):
-    pass
+    new = []
+    for value in x:
+        if value not in new:
+            new.append(value)
+    return new
+
+
+print(del_dups_1([5, 9, 9, 9, 6]))
+
+
+import collections
 
 
 def del_dups_2(x):
-    pass
+    return list(collections.OrderedDict.fromkeys(x))
 
+
+print(del_dups_2([1, 2, 3, 4, 3, 4]))
 
