8ce8ea218f567d44e32a3fd8d6a2cd8b2b0eff4d
Pratik-sys <pithore.pratik@gmail.com>
2020-01-19 16:46:45 +0530

    List:update_soln:index_non_zero
    Signed-off-by: Pratik-sys <pithore.pratik@gmail.com>

--- Lists/index_non_zero.py
+++ Lists/index_non_zero.py
@@ -1,6 +1,15 @@
 # Index of Non-Zero elements in a list
-# Write a program find positions of all the integers other than 0.
+# Write a program to find positions of all the integers other than 0.
 
 """
 [6, 7, 0, 1, 0, 2, 0, 12] --->  [0, 1, 3, 5, 7]
 """
+
+def positions(x):
+    a = []
+    for i,v in enumerate(x):
+        if v !=0:
+            a.append(i)
+    return a
+
+print(positions([6, 7, 0, 1, 0, 2, 0, 12]))
