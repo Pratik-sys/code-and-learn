a488a01d79788eeadfa639180b791a4817252c02
Pratik-sys <pithore.pratik@gmail.com>
2020-01-30 11:07:05 +0530

    sort the random genereated list
    Signed-off-by: Pratik-sys <pithore.pratik@gmail.com>

--- Modules/sorted_random_list.py
+++ Modules/sorted_random_list.py
@@ -8,10 +8,13 @@ Output: [102, 118, 124, 131, 140, 148, 161, 166, 176, 180]
 Input: num:5, start:1, end:100
 Output: [37, 49, 64, 84, 95]
 """
-
-def sorted_random(num, start=default_value, end=default_value):
+import random
+def sorted_random(num, start, end):
     '''
     To-do: Change a default value to the desirable value
     start < end, always
     '''
-    pass
+   
+    return sorted(random.randint(start, end) for i in range(num))
+    
+print(sorted_random(10, 1, 200))
