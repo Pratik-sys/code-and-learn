8dee206dd7efc0267765dac030f6ba9e1e77639d
Pratik-sys <pithore.pratik@gmail.com>
2020-02-04 00:12:43 +0530

    string:del the duplicates
    Signed-off-by: Pratik-sys <pithore.pratik@gmail.com>

--- Strings/del_dups_str.py
+++ Strings/del_dups_str.py
@@ -12,5 +12,14 @@ ramayan ---> ramyn
 
 
 def remove_dup_string(strng):
-    pass
+    lst = list(strng)
+    new = []   
+    for value in lst:
+        if value not in new:
+            new.append(value)
+            new_str = ''. join([str(i) for i in new])
+    return new_str
 
+
+
+print(remove_dup_string("programming"))
