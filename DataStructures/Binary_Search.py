4fd31bca659766bd3157e026af994732e5bc5fbf
Pratik-sys <pithore.pratik@gmail.com>
2020-02-16 09:08:32 +0530

    ittrative binary search

--- DataStructures/Binary_Search.py
+++ DataStructures/Binary_Search.py
@@ -1,2 +1,24 @@
-def binary_search():
-    pass
+def binary_search(a,x):
+    """Efficient program for searching the element in an array 
+       with ittrative way with O(logn) time complexity
+    """
+    start = 0 
+    end = len(a)-1
+
+    while start<= end:
+        mid = (start+end)//2 
+        if x == a[mid]:
+            return mid
+        elif x < a[mid]:
+            end = mid-1
+        else:
+            start = mid+1
+    return -1
+
+a = [2, 3, 4, 10, 40]
+x = int(input("Enter element:"))
+result = binary_search(a,x)
+if result != -1:
+    print(f'element {x} is present at index {result}')
+else:
+    print(f'element {x} does not exist in an array')
