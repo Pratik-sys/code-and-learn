9e2d9a434ea74b16f3de7f36012de7e2f567db0f
Pratik-sys <pithore.pratik@gmail.com>
2020-03-01 13:16:55 +0530

    update: wrong code
    Signed-off-by: Pratik-sys <pithore.pratik@gmail.com>

--- DataStructures/Binary_Search/count_occurances_of_duplicates.py
+++ DataStructures/Binary_Search/count_occurances_of_duplicates.py
@@ -1,7 +1,7 @@
 # Given a sorted array of n elements, possibly with duplicates, find the number of occurrences of the target element.
 
-def first_occ(a,x):
-
+def count_occ(a,x,serchfirst):
+    """ gives the count of occurance of element in the array """
     start = 0 
     end = len(a)-1
     result = - 1
@@ -10,18 +10,22 @@ def first_occ(a,x):
 
         if x == a[mid]:
             result = mid
-            end = mid - 1
+            if serchfirst:
+                end = end - 1
+            else:
+                start = mid + 1
         elif x < a[mid]:
             end = mid -1
         else:
             start = mid+1
     return result
 
-a = [2,4,10,10,10,18,20]
-x = 10
-pac = first_occ(a,x)
+a = [1,1,3,3,5,5,5,5,5,9,9,11]
+x = 5
+first = count_occ(a,x,True)
 
-if pac != -1:
-    print(f'first occurance of {x} is at index {pac}')
+if first == -1:
+    print(f'count  of {x} is 0')
 else:
-    print("Element not found")  
+    last = count_occ(a,x,False)
+    print(f'count of {x} is {last-first+1}')
