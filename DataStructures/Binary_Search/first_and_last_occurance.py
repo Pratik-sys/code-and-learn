a304659fb22d616daf4678a518fa9c3d0f37baac
Pratik-sys <pithore.pratik@gmail.com>
2020-03-03 13:34:35 +0530

    update solution : first and last occurance
    Signed-off-by: Pratik-sys <pithore.pratik@gmail.com>

--- DataStructures/Binary_Search/first_and_last_occurance.py
+++ DataStructures/Binary_Search/first_and_last_occurance.py
@@ -2,5 +2,32 @@
 # Your algorithm's runtime complexity must be in the order of O(log n).
 # If the target is not found in the array, return [-1, -1].
 
-def searchRange():
-    pass
+def occurance(a,x,serchfirst):
+    """ gives the count of occurance of element in the array """
+    start = 0 
+    end = len(a)-1
+    result = - 1
+    while start <= end:
+        mid = (start+end)//2
+
+        if x == a[mid]:
+            result = mid
+            if serchfirst:
+                end = end - 1
+            else:
+                start = mid + 1
+        elif x < a[mid]:
+            end = mid -1
+        else:
+            start = mid+1
+    return result
+
+a = [1,1,3,3,5,5,5,5,5,9,9,11]
+x = 5
+first = occurance(a,x,True)
+last = occurance(a,x,False)
+
+if first and last == -1:
+    print(f'{x} occured 0 times')
+else:
+    print(f'Element {x}"s first occurane is {first} and last occurance is {last}')
