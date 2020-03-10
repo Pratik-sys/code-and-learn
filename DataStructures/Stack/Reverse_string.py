4b14d4eb9896dd4285efb05953e92ee5bc211258
Pratik-sys <pithore.pratik@gmail.com>
2020-03-10 14:34:02 +0530

    Stack: solution: reverse string using stack
    Signed-off-by: Pratik-sys <pithore.pratik@gmail.com>

--- DataStructures/Stack/Reverse_string.py
+++ DataStructures/Stack/Reverse_string.py
@@ -1,4 +1,19 @@
+#!/usr/bin/python3
+
 # Reverse the string using stack operation .
 
-def reverse_string():
-    pass
+def reverse_string(inpt_str):
+    """ The fucntion reverses the string using stack operation."""
+    stack = []
+    for i in range(len(inpt_str)):
+        stack.append(inpt_str[i])
+        # As append is inbuilt function for list, shows the similar result as push 
+    string = ""
+    for _ in range(len(inpt_str)):
+
+        string+= stack.pop()
+        # The emptry string getss added with the everty signe item poped
+       
+    return string
+
+print(reverse_string("HELLO"))
