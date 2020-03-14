8f99681f6897f9c7ff4470a1ecee4a76c903d94a
Pratik-sys <pithore.pratik@gmail.com>
2020-03-15 00:06:08 +0530

    add some comments
    Signed-off-by: Pratik-sys <pithore.pratik@gmail.com>

--- DataStructures/Stack/Convert_decimal_integer_to_binary.py
+++ DataStructures/Stack/Convert_decimal_integer_to_binary.py
@@ -3,13 +3,15 @@
 # Convert deciaml to binary using Stack Operation 
 
 def Convert(num):
-    stack = []
+    """ The functions returns the binary format of the interger """
+    stack = [] # using empty list as stack.
     while num > 0 :
         reminder = num % 2
         stack.append(reminder)
         num = num // 2
     bin_num = ""
     for _ in range(len(stack)):
-        bin_num+=str(stack.pop())
+        bin_num+=str(stack.pop()) 
+        # combining the pope element with empty string to get the output.
     return bin_num
 print(Convert(45))
