42d9f805ba2f379ccde83aa913f6b7866407e1b1
Pratik-sys <pithore.pratik@gmail.com>
2020-03-15 00:00:46 +0530

    Stack:update the solution
    Signed-off-by: Pratik-sys <pithore.pratik@gmail.com>

--- DataStructures/Stack/Convert_decimal_integer_to_binary.py
+++ DataStructures/Stack/Convert_decimal_integer_to_binary.py
@@ -1,4 +1,15 @@
+#!/usr/bin/python3
+
 # Convert deciaml to binary using Stack Operation 
 
-def Convert():
-    pass
+def Convert(num):
+    stack = []
+    while num > 0 :
+        reminder = num % 2
+        stack.append(reminder)
+        num = num // 2
+    bin_num = ""
+    for _ in range(len(stack)):
+        bin_num+=str(stack.pop())
+    return bin_num
+print(Convert(45))
