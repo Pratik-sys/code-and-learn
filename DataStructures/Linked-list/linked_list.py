51247b93414f639666af4403f67a015b36af5cd2
Pratik-sys <pithore.pratik@gmail.com>
2020-03-21 08:36:56 +0530

    Add prepend and insert_after function to linked list
    Signed-off-by: Pratik-sys <pithore.pratik@gmail.com>

--- DataStructures/Linked-list/linked_list.py
+++ DataStructures/Linked-list/linked_list.py
@@ -27,12 +27,29 @@ class Linked_list:
             last_node = last_node.next 
         last_node.next = new_node
 
+    # prepend fuction inserts the new node at the begining of the linked list 
+    def prepend(self, data):
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node 
+
+    # insert after function inserts the new node bweteen the head node and previous node.
+    def insert_after(self, prev_node, data):
+        if not prev_node:
+            print("previous node doesn't exist")
+            return
+        new_node = Node(data)
+        new_node.next = prev_node.next
+        prev_node.next = new_node
+
+
 
 l = Linked_list()
 l.append("A")
 l.append("B")
 l.append("C")
-l.apend("D")
-
-
+l.append("D")
+l.prepend("E")
+l.insert_after(l.head.next, "F")
 l.print_list()
+
